81c03dd83c55de0732dbc2bc7998e63f76c6979f
subhasis-karmakar <36099213+subhasis-karmakar@users.noreply.github.com>
2018-03-15 18:35:36 +0530

    Update installapache.sh

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-apt-get -y install apache2 > /dev/null 2>&1
-apt-get -y install php7.0 php7.0-* libapache2-mod-php7.0 > /dev/null 2>&1
+rm -rf /var/www/html/index.html
+#apt-get -y install apache2 > /dev/null 2>&1
+#apt-get -y install php7.0 php7.0-* libapache2-mod-php7.0 > /dev/null 2>&1
